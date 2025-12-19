2d90acaf4252b21c1497c344e47d048aaeba594c
HeyKayDi <153514122+HeyKayDi@users.noreply.github.com>
2025-12-20 02:48:19 +0700

    Update vite.config.ts

--- source/vite.config.ts
+++ source/vite.config.ts
@@ -3,13 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  // Load các biến môi trường từ file .env
+  // Load environment variables from the .env file
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
     define: {
-      // Định nghĩa process.env để code cũ vẫn hoạt động mà không cần sửa đổi
+      // Define process.env so that the old code continues to work without modification.
       'process.env': env
     }
   };
+
 });
